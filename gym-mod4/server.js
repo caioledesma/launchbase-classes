f3abda25e53b2f91716da0f1243db3c945750e57
Caio Ledesma <42016457+caioledesma@users.noreply.github.com>
2020-09-15 07:41:16 -0300

    restore files and update 15-september

--- gym-mod4/server.js
+++ gym-mod4/server.js
@@ -32,5 +32,5 @@ nunjucks.configure("views", {
 //chamamos a const server e coloca ele pra ouvir a função listen    
 // a função listen recebe dois parâmetros, a porta e outra função dentro
 server.listen(5000, function() {   
-    console.log('Run! Its a marathon, not a srpint!')
+    console.log('Run! Its a marathon, not a sprint!')
 })
